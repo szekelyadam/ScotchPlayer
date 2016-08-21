53e951ce0850e9961845601702e0cafe3ad3171d
szekelyadam <info@szekelyadam.hu>
2016-08-21 18:20:54 +0200

    Compontents and styles

--- app/app.js
+++ app/app.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Search from './components/search.component';
+import Details from './components/details.component';
+import Player from './components/player.component';
+import Progress from './components/progress.component';
+import Footer from './components/footer.component';
 
 // Search component
 class App extends React.Component {
   render() {
     return (
-      <Search />
+      <div>
+        <Search />
+        <Details title={'Track title'} />
+        <Player />
+        <Progress
+          position={'0.3'}
+          elapsed={'00:00'}
+          total={'0:40'}
+        />
+        <Footer />
+      </div>
     );
   }
 }
---
+++ app/components/progress.component.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Progress extends React.Component {
+  render() {
+    return(
+      <div className="progress">
+        <span className="player__time-elapsed">{this.props.elapsed}</span>
+        <progress
+          value={this.props.position}
+          max="1"
+        />
+        <span className="player__time-total">{this.props.total}</span>
+      </div>
+    );
+  }
+}
+
+export default Progress;
